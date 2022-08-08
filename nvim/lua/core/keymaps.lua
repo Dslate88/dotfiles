f1f87861a409dce9b7b05f8d3b4ba4f49749d96f
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-08-08 14:23:35 -0600

    keymap.lua organization

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,3 +1,7 @@
+-----------------------------------------------------------
+-- general
+-----------------------------------------------------------
+-- keymap fn
 local function map(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
   if opts then
@@ -6,13 +10,8 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-
 -- Change leader to a space
 vim.g.mapleader = ' '
-
------------------------------------------------------------
--- Neovim shortcuts
------------------------------------------------------------
 
 -- Disable arrow keys
 map("", "<up>", "<nop>")
@@ -20,13 +19,43 @@ map('', '<down>', '<nop>')
 map('', '<left>', '<nop>')
 map('', '<right>', '<nop>')
 
--- Telescope
-map("n", "<leader>ff", ":Telescope find_files<cr>")
-map("n", "<leader>fg", ":Telescope live_grep<cr>")
+-----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
 
---harpoon
+
+-----------------------------------------------------------
+-- lspconfig shortcuts
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- harpoon shortcuts
+-----------------------------------------------------------
 map("n", "<leader>hu", ":lua require('harpoon.ui').toggle_quick_menu()<cr>")
 map("n", "<leader>ha", ":lua require('harpoon.mark').add_file()<cr>")
+
+-----------------------------------------------------------
+-- telescope shortcuts
+-----------------------------------------------------------
+map("n", "<leader>ff", ":Telescope find_files<cr>")
+map("n", "<leader>fg", ":Telescope live_grep<cr>")
 --add leader ctrl [1/2/3/4/5] for quick access to needed files
 --OR instead of that use the nav_next & nav_prev
 --the send to terminal command will be great for rapid testing of code `gottoTerminal`
+
+-----------------------------------------------------------
+--
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+--
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+--
+-----------------------------------------------------------
+
+
+
+
+
