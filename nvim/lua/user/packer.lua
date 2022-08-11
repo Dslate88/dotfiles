a6f4a695706ee26b8c8f1f9d6ab98e7106475a8c
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-08-10 19:18:58 -0600

    explicitly callout ripgrep needs external install

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -15,7 +15,7 @@ return require("packer").startup(function(use)
         tag = "0.1.0",
         requires = {
             {"nvim-lua/plenary.nvim"},
-            {"BurntSushi/ripgrep"}
+            {"BurntSushi/ripgrep"} --brew install ripgrep
         }
     }
     -- randos
