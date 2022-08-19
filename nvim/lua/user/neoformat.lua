acc3bab68c76a5bbe17551ec306c78a0f12debfd
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-08-18 23:15:57 -0600

    disable python neogit formatting for now

--- nvim/lua/user/neoformat.lua
+++ nvim/lua/user/neoformat.lua
@@ -1,3 +1,3 @@
 --this is broke, but the autocommand is running
-vim.g.neoformat_python_autopep8 = {"exe": "black", "args": ["-"], "replace": 1, "stdin": 1}
+-- vim.g.neoformat_python_autopep8 = {"exe": "black", "args": ["-"], "replace": 1, "stdin": 1}
 
