370d4e957c4a7d3b7a5f97b38d439a7274a1a6f1
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-08-18 23:22:38 -0600

    use autofmt for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,10 +12,10 @@ require("user/neogit")
 require("user/telescope") --needs live_grep config still
 require("user/lsp")
 
---require("user/neoformat")--see bugs in lua file
+require("user/neoformat")--see bugs in lua file
 require("user/cmp")
 require("user/mason")
 require("user/comment")
 require("user/lualine")
 require("user/treesitter")
-
+require("user/whichkey")
