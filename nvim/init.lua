068b2c56f41b76bfb087a5a2133291b59cf082e9
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-08-20 20:46:55 -0600

    init.lua clean up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,12 @@
--- vim.cmd("set termguicolors")
-
-
 require("user/packer")
 require("core/options")
 require("core/colors")
 require("core/autocmd")
 require("core/keymaps")
 
-
 require("user/neogit")
 require("user/telescope") --needs live_grep config still
 require("user/lsp")
-
 require("user/neoformat")--see bugs in lua file
 require("user/cmp")
 require("user/mason")
