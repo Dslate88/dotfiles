145649dac485c8c5f368af2eebebcb4d929ff30a
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-10-16 00:25:47 -0600

    added floating task terminal keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -72,7 +72,7 @@ map("n", "<leader>wc", ":close<cr>", { noremap = false, silent = true })
 map("t", "<Esc>", "<C-\\><C-n>")
 map("n", "<leader>tt", ":ToggleTerm<cr>")
 map("n", "<leader>ta", ":ToggleTermToggleAll<cr>")
-
+map("n", "<leader>tw", ":1TermExec direction=vertical size=90 cmd=\"task\"<cr>")
 
 
 -----------------------------------------------------------
