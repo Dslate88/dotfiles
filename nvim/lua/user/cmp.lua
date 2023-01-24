29d9a8eb0463c0bb908f023708c7fd021e675330
Devin Slate <sl8t3@hotmail.com>
2023-01-23 17:03:21 -0700

    improved lsp for pythong

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -81,7 +81,18 @@ local cmp = require'cmp'
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    settings = {
+      python = {
+        analysis = {
+          typeCheckingMode = "off",
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = false,
+          reportUnusedVariable = true,
+        },
+      },
+    },
+
   }
 
   require('lspconfig')['terraformls'].setup {
