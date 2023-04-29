976763d90cda8c9c2d9093a0fd895ca1f41ced87
Devin Slate <sl8t3@hotmail.com>
2023-04-29 14:02:06 -0600

    update_capabilites deprecated bugfix

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -78,7 +78,7 @@ local cmp = require'cmp'
   })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   require('lspconfig')['pyright'].setup {
     capabilities = capabilities,
