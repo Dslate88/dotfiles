a363e7b5194ac3efab86b29a7973031191d0bae5
Devin Slate <sl8t3@hotmail.com>
2025-09-14 10:19:56 -0600

    rm provider factory for single baml endpoint

--- nvim/lua/user/ai_tools/init.lua
+++ nvim/lua/user/ai_tools/init.lua
@@ -1,9 +1,10 @@
 local M = {}
 
 M.config = require("user.ai_tools.config")
-M.providers = require("user.ai_tools.providers")
-M.scripts = require("user.ai_tools.scripts")
 M.utils = require("user.ai_tools.utils")
 M.logger = require("user.ai_tools.logger")
+
+-- Optional: expose scripts registry for convenience
+M.scripts = require("user.ai_tools.scripts")
 
 return M
--- nvim/lua/user/ai_tools/scripts/init.lua
+++ nvim/lua/user/ai_tools/scripts/init.lua
@@ -1,6 +1,4 @@
-local M = {}
-
-M.harpoon_list = require("user.ai_tools.scripts.harpoon_list")
-M.chat = require("user.ai_tools.scripts.chat")
-
-return M
+return {
+	chat = require("user.ai_tools.scripts.chat"),
+	harpoon_list = require("user.ai_tools.scripts.harpoon_list"),
+}
